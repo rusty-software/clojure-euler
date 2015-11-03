2733983466df89ae4f10432aaa916e0d6885f33a
Rusty Bentley <mycoding.alert@gmail.com>
2015-11-03 17:28:49 -0600

    fixing docstring

--- src/clojure_euler/core.clj
+++ src/clojure_euler/core.clj
@@ -1,6 +1,6 @@
 (ns clojure-euler.core)
 
 (defn problem-001
-  "Returns sum of all multiples of 3 or five below arg value."
+  "Returns sum of all multiples of 3 or 5 below arg value."
   [n]
   23)
