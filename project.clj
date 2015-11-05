943a6db657794e201aa532a5a8fc4873dfa83fb4
Rusty Bentley <mycoding.alert@gmail.com>
2015-11-05 06:50:03 -0600

    adding test refresh and ultra to project

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject clojure-euler "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :description "Solutions to projecteuler.net problems, in clojure"
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.11.0"]
+                             [venantius/ultra "0.3.4"]]
+                   :ultra {:color-scheme :default }}}
+  :test-selectors {:default (constantly true)
+                   :single :single})
