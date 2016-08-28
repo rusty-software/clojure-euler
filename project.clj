cad2a6ac48f0c19366a79cd6ff0391675e873306
Rusty Bentley <mycoding.alert@gmail.com>
2016-08-27 21:06:18 -0500

    updating clojure version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clojure-euler "0.1.0-SNAPSHOT"
   :description "Solutions to projecteuler.net problems, in clojure"
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.11.0"]
                              [venantius/ultra "0.3.4"]]
                    :ultra {:color-scheme :solarized-dark }}}
